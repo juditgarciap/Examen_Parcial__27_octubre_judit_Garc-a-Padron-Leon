b8ced1781538bf79a9b8a4a5f082fbcc512a4fff
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-10-27 20:08:33 +0200

    ej2

--- ej2.py
+++ ej2.py
@@ -1,1 +1,8 @@
-#hay un metodo para ordenar listas 
+#hay un metodo para ordenar listas mergesort
+def mergesort (lista):
+    if (len(lista) <= 1):
+        return lista
+    else:
+        medio= len(lista)//2
+        izquierda = []
+        for i in range (0,medio):

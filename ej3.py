a1e60d0c3830129cb0b60840f673140050898146
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-10-27 19:28:42 +0200

    ej3

---
+++ ej3.py
@@ -0,0 +1,19 @@
+#ejercicio parecido a vehiculos, hecho en este repositorio https://github.com/juditgarciap/EjPOO.git, el codigo lo he reutilizado
+
+class Alumnos():
+    def __innit__ (self, nombre,nota):
+        self.nombre = nombre
+        self.nota = nota 
+    def __str__ (self):
+        return 'nombre del alumno {}, nota del alumno {}'
+
+alumnos = []
+def calificacion (lista_alumnos):
+    for alumno in lista_alumnos:
+
+
+
+
+
+
+

b4407eaf2c8d7010da219a296baae458487ed1b6
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-10-27 20:31:42 +0200

    ej2 todaviiiiia

--- ej2.py
+++ ej2.py
@@ -1,4 +1,22 @@
-#hay un metodo para ordenar listas mergesort
+#en los apuntes podemos tambien encontrar este algoritmo y creo que tambien se puede resolver con este
+def centinela(lista,buscado):
+
+pos
+
+
+
+
+
+
+
+
+
+
+#hay un metodo para ordenar listas mergesort ( no se si esto se debe usar así)
+
+from csv import list_dialects
+
+
 def mergesort (lista):
     if (len(lista) <= 1):
         return lista
@@ -12,6 +30,30 @@ def mergesort (lista):
         if (izquierda[medio-1]<= derecha[0]):
            izquierda += derecha
            return izquierda
-        resultado = merge(izquierda,derecha) 
+        resultado = mergesort(izquierda,derecha) 
         return resultado 
-        
+
+#función para mezclar las listas
+def merge(izquierda,derecha):
+    lista_mezcla =[]
+    while (len(izquierda>0)) and (len(derecha> 0):
+        if (izquierda[0]< derecha[0]):
+            lista_mezcla.append (izquierda.pop(0)
+        else:
+            lista_mezcla.append(derecha.pop(0))
+    if(len(izquierda)>0):
+        lista_mezcla += izquierda 
+    if (len(derecha)>0):
+        lista_mezcla += derechaç
+    return lista_mezcla
+
+
+lista = [8, 50, 210, 80, 145, 333, 70, 30] 
+print(mergesort(lista))
+
+
+
+
+if __name__ == "__name__":
+    import doctest 
+    doctest.tesmode()
